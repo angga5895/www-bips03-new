69f960aa87d2dc31c29cadc7084984dc1d27d2af
zakyymuh <zakyymuh123@gmail.com>
2020-07-02 16:28:00 +0700

    Update Modal Trial desain and text

--- src/app_modals/modal_trial.js
+++ src/app_modals/modal_trial.js
@@ -5,6 +5,7 @@ import {ResizeResponsive} from "../app_pages/mainPage";
 // import {ForgotModal} from "../app_pages/loginPage";
 
 import $ from "jquery";
+import {Checkbox} from "semantic-ui-react";
 
 
 class ModalTrial extends React.Component{
@@ -19,14 +20,25 @@ class ModalTrial extends React.Component{
     }
     state = {
         selected: 1,
+        selectedMigrate: true,
+    }
+    migrate = () => {
+        if(this.selectedMigrate == false){
+            this.closeClickNoAlert();
+        }else{
+            if(this.selected == 1){
+                this.setState({selected: 4});
+            }else{
+                this.setState({selected: 3});
+            }
+        }
     }
     render() {
         return (
             <>
                 <AppFrameAction ref="frameAction" />
                 <div className="f-12">
-                    <div>
-                        <div className={`col-sm-6 pb-3 text-white ${this.state.selected < 3?"d-block":"d-none"}`}>
+                        <div className={`col-sm-6 pb-3 ${this.state.selected < 3?"d-block":"d-none"}`}>
                             <div
                                 className={`px-0 pt-3
                                                   ${this.state.selected == 1?"livetradeMenuActive":"livetradeMenu"}`}
@@ -50,90 +62,78 @@ class ModalTrial extends React.Component{
                         </div>
                     </div>
                     <div className={"col-sm-12 bg-gradi row ml-0 pr-0"}>
-                        <div
-                            className={`px-0 pt-3 col-sm-8 px-3 py-4 ${this.state.selected == 1?"d-block":"d-none"}`}
+                        <div className={`px-0 pt-3 col-sm-8 px-3 py-4 ${this.state.selected == 1?"d-block":"d-none"}`}
                         >
-                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Selamat Datang di DXTRADE</h2>
+                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Peringatan Notifikasi </h2>
                             <br/>
                             <p className={"p-migrate"}>
-                                Nasabah yang terhormat terimakasih atas kepercayaan anda menggunakan Trading bersama DT nextG.
-                                Kami informasikan bahwa aplikasi DT NextG akan segera kami ganti dengan aplikasi baru yaitu DxTrade.<br/>
-                                Saat ini DxTrade hanya dapat diakses melalui web browser. Versi lain dari aplikasi ini
-                                akan segera tersedia pada bulan September 2020.
+                                Terimakasih anda telah bersedia kami migrasikan ke aplikasi Dxtrade.<br/>
+                                Berikut ini informasi yang harus kami sampaikan :<br/><br/>
+
+                                1. Account anda akan active mulai H + , jika pilih setuju sebelum Pukul 15.00 WIB.<br/>
+                                2. Setelah Account DXTrade anda kami Active-kan, Account anda tidak dapat di pergunakan kembali di aplikasi DT NextG.<br/>
+                                3. Saat ini aplikasi DXTrade hanya dapat di akses melalui browser di alamat <a href={"https://dxtrade.bahana.co.id/"} target={"_blank"}>https://dxtrade.bahana.co.id/</a> dan
+                                aplikasi DXTrade belum dapat di akses oleh mobile app device.<br/>
+                                4. Aplikasi DXTrade versi mobile akan tersedia pada bulan September 2020 setelah mendapatkan sertifikasi dari bursa. <br/>
                             </p>
                             <p className={"p-migrate"}>
-                                Anda dapat mempergunakan User DT NextG anda di aplikasi DxTrade.
-                                Jika anda berkenan untuk kami migrasikan dari DT NextG ke DxTrade saat ini,
-                                silahkan pilih "Setuju Migrasi" atau "Masih ingin mencoba" bila belum anda tidak dapat
-                                lagi mengakses DT NextG, bilamana sudah dimigrasikan.<br/>
+                                Apakah anda yakin untuk Migrasi
                             </p>
-                            <p className={"p-migrate"}>
-                                Seluruh nasabah DT NextG yang masih menggunakan aplikasi DT NextG akan kami migrasikan ke aplikasi DxTrade pada akhir agustus 2020.
-                                <br/><br/>
-                                Terimakasih.
-                            </p>
-                            <div className={"text-center col-sm-12"}>
-                                <button onClick={()=>this.setState({selected:4})} id="click_migrate" type="submit"
-                                        className="btn btn-primary form-control py-0 col-sm-3">
-                                    <span id={"text-login"}>Migrasikan Saya</span>
-                                </button>
-                                <br/><br/>
-                                <u className={"click-pointer"} onClick={this.closeClickNoAlert}>Mungkin nanti</u>
+                            <div className={"row"}>
+                                <div className={"text-center col-sm-9"}>
+                                    <div className={"text-left"}>
+                                        <Checkbox label='Ya' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} defaultChecked checked={(this.state.selectedMigrate)?true:false}/>
+                                    </div>
+                                    <div className={"text-left"}>
+                                        <Checkbox label='Tidak' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} checked={(this.state.selectedMigrate)?false:true}/>
+                                    </div>
+                                </div>
+                                <div className={"col-sm-3 text-center"}>
+                                    <button className="ui icon button red go-button circular" onClick={this.migrate}><i aria-hidden="true" className="play icon"></i>
+                                    </button><br/>
+                                    <span className={"f-18 text-danger pointer"} onClick={this.migrate}>OK</span>
+                                </div>
                             </div>
                         </div>
                         <div
                             className={`px-0 pt-3 col-sm-8 px-3 py-4 ${this.state.selected == 2?"d-block":"d-none"}`}
                         >
-                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Welcome to DX TRADE</h2>
-                            <br/>
-                            <p className={"p-migrate"}>
-                                Dear Customers, thank you for your trust using trading with DT NextG.<br/>
-                                We inform you that the DT NextG application will be replaced soon with newest application called DXTrade.
-                                Currently DXTRADE can only accessed via web browser and for mobile application version would be available in September 2020.
-                            </p>
-                            <p className={"p-migrate"}>
-                                You can use your DT NextG account in DXTrade Application.
-                                If you wish to migrate from DT NextG to DXTRADE, please select "Agree to migrate".
-                                If you wish to continue exploring please select "Still want to try".
-                                You can not longer access DT NextG, if it has been migrated.<br/>
-                            </p>
-                            <p className={"p-migrate"}>
-                                All DT NextG customers who still use DT NextG application will be migrated to DXTRADE applcation at the end of August 2020.
-                                <br/><br/>
-                                Thank you.
-                            </p>
-                            <div className={"text-center col-sm-12"}>
-                                <button onClick={()=>this.setState({selected:3})} id="click_migrate" type="submit"
-                                        className="btn btn-primary form-control py-0 col-sm-3">
-                                    <span id="text-login" >Migrate Me</span>
-                                </button>
-                                <br/><br/>
-                                <u className={"click-pointer"} onClick={this.closeClickNoAlert}>Maybe Later</u>
-                            </div>
-                        </div>
-                        <div
-                            className={`px-0 pt-3 col-sm-12 px-3 py-4 ${this.state.selected == 3?"d-block":"d-none"}`}
-                        >
-                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>THANK YOU</h2>
+                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Notification Alert</h2>
                             <br/>
                             <p className={"p-migrate"}>
                                 Thank you for being willing to migrate to the Dxtrade application.<br/>
-                                Your account will be active starting H +, if you choose agree before 15.00 WIB.
-                                After you activate your DXTrade account, your account cannot be reused in the DT NextG
-                                application.
+                                Here is the information that we have to submit:<br/><br/>
+                                1. Your account will be active starting H +, if you choose agree before 15.00 WIB.<br/>
+                                2. After you activate your DXTrade account, your account cannot be reused in the DT NextG application.<br/>
+                                3. At present the DXTrade application can only be accessed through a browser at the address <a href={"https://dxtrade.bahana.co.id/"} target={"_blank"}>https://dxtrade.bahana.co.id/</a> <br/>
+                                and the DXTrade application cannot be accessed by a mobile app device.<br/>
+                                4. The mobile version of the DXTrade application will be available in September 2020 after obtaining certification from the Market exchange<br/>
                             </p>
                             <p className={"p-migrate"}>
-                                The mobile version of the DXTrade application will be available in September 2020 after
-                                obtaining certification from the exchange.
+                                Are u sure about migration?
                             </p>
+                            <div className={"row"}>
+                                <div className={"text-center col-sm-9"}>
+                                    <div className={"text-left"}>
+                                        <Checkbox label='Yes' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} defaultChecked checked={(this.state.selectedMigrate)?true:false}/>
+                                    </div>
+                                    <div className={"text-left"}>
+                                        <Checkbox label='No' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} checked={(this.state.selectedMigrate)?false:true}/>
+                                    </div>
+                                </div>
+                                <div className={"col-sm-3 text-center"}>
+                                    <button className="ui icon button red go-button circular" onClick={this.migrate}><i aria-hidden="true" className="play icon"></i>
+                                    </button><br/>
+                                    <span className={"f-18 text-danger pointer"} onClick={this.migrate}>OK</span>
+                                </div>
+                        </div>
+                        </div>
+                        <div
+                            className={`px-0 pt-3 col-sm-12 px-3 py-4 text-center ${this.state.selected == 3?"d-block":"d-none"}`}
+                        >
                             <p className={"p-migrate"}>
-                                At present the DXTrade application can only be accessed through a browser at the address
-                                &quot;https://dxtrade.bahana.co.id/&quot; and the DXTrade application cannot be accessed by a
-                                mobile app device.<br/>
-                            </p>
-                            <p className={"p-migrate"}>
-                                The mobile version of the DXTrade application will be available in September 2020 after
-                                obtaining certification from the exchange.
+                                Thank you for evaluating your current DXTrade application, your current DXTrade account is a trial.<br/>
+                                Please select a migration when you login to be able to activate your account on DXTrade.<br/>
                             </p>
                             <div className={"text-center col-sm-12"}>
                                 <button id="click_migrate" type="submit" onClick={this.closeClickNoAlert}
@@ -143,28 +143,14 @@ class ModalTrial extends React.Component{
                             </div>
                         </div>
                         <div
-                            className={`px-0 pt-3 col-sm-12 px-3 py-4 ${this.state.selected == 4?"d-block":"d-none"}`}
+                            className={`px-0 pt-3 col-sm-12 px-3 py-4 text-center ${this.state.selected == 4?"d-block":"d-none"}`}
                         >
-                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Terimakasih</h2>
                             <br/>
                             <p className={"p-migrate"}>
                                 Terimakasih anda telah bersedia kami migrasikan ke aplikasi Dxtrade.
                                 <br/>
-                                Saat ini DxTrade hanya dapat diakses melalui web browser. Versi lain dari aplikasi ini
-                                akan segera tersedia pada bulan September 2020.
-                                Berikut ini informasi yang harus kami sampaikan :
-                                Account anda akan active mulai H + , jika pilih setuju sebelum Pukul 15.00 WIB.
-                                Setelah Account DXTrade anda kami Active-kan, Account anda tidak dapat di pergunakan
-                                kembali di aplikasi DT NextG.
-                            </p>
-                            <p className={"p-migrate"}>
-                                Saat ini aplikasi DXTrade hanya dapat di akses melalui browser di alamat &quot;
-                                https://dxtrade.bahana.co.id/ &quot; dan aplikasi DXTrade belum dapat di akses oleh mobile app
-                                device.
-                            </p>
-                            <p className={"p-migrate"}>
-                                Aplikasi DXTrade versi mobile akan tersedia pada bulan September 2020 setelah
-                                mendapatkan sertifikasi dari bursa.
+                                Saat ini account DXTrade anda bersifat percobaan.<br/>
+                                Silahkan pilih migrasi saat anda login untuk dapat kami aktifasikan account anda ke DXTrade anda.
                             </p>
                             <div className={"text-center col-sm-12"}>
                                 <button id="click_migrate" type="submit" onClick={this.closeClickNoAlert}
@@ -173,12 +159,6 @@ class ModalTrial extends React.Component{
                                 </button>
                             </div>
                         </div>
-
-
-                        <div className={`col-sm-4 ${this.state.selected < 3? "d-block":"d-none"} bg-grey` }>
-                        </div>
-
-                    </div>
                 </div>
             </>
         );

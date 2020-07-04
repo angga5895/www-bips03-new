f8c6ef218c3d1beeb87a5223dc771a7aa49331b1
zakyymuh <zakyymuh123@gmail.com>
2020-07-04 09:53:13 +0700

    Update text trial infomration

--- src/app_modals/modal_trial.js
+++ src/app_modals/modal_trial.js
@@ -62,19 +62,26 @@ class ModalTrial extends React.Component{
                         </div>
                     </div>
                     <div className={"col-sm-12 bg-gradi row ml-0 pr-0"}>
-                        <div className={`px-0 pt-3 col-sm-8 px-3 py-4 ${this.state.selected == 1?"d-block":"d-none"}`}
+                        <div className={`px-0 pt-3 col-sm-12 col-xs-12 px-3 py-4 ${this.state.selected == 1?"d-block":"d-none"}`}
                         >
-                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Peringatan Notifikasi </h2>
+                            <h3 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Selamat Datang di DX Trade </h3>
                             <br/>
                             <p className={"p-migrate"}>
-                                Terimakasih anda telah bersedia kami migrasikan ke aplikasi Dxtrade.<br/>
-                                Berikut ini informasi yang harus kami sampaikan :<br/><br/>
+                                Terima kasih atas kepercayaan Anda untuk senantiasa menggunakan Online Trading Bahana Sekuritas.<br/><br/>
+                                Sebagai bentuk komitmen kami untuk meningkatkan kenyamanan Anda,
+                                maka dengan kami informasikan bahwa kami telah melakukan pembaharuan sistem <i>online trading</i> kami
+                                menjadi DX Trade. Saat ini DX Trade hanya dapat digunakan melalui <i>web browser</i>.
+                                Platform DX Trade di <i>platform</i> lainnya akan segera tersedia pada bulan September 2020.<br/><br/>
 
-                                1. Account anda akan active mulai H + , jika pilih setuju sebelum Pukul 15.00 WIB.<br/>
-                                2. Setelah Account DXTrade anda kami Active-kan, Account anda tidak dapat di pergunakan kembali di aplikasi DT NextG.<br/>
-                                3. Saat ini aplikasi DXTrade hanya dapat di akses melalui browser di alamat <a href={"https://dxtrade.bahana.co.id/"} target={"_blank"}>https://dxtrade.bahana.co.id/</a> dan
-                                aplikasi DXTrade belum dapat di akses oleh mobile app device.<br/>
-                                4. Aplikasi DXTrade versi mobile akan tersedia pada bulan September 2020 setelah mendapatkan sertifikasi dari bursa. <br/>
+                                Anda sudah dapat menggunakan DX Trade dengan menggunakan User ID yang Anda miliki.<br/>
+                            <ul>
+                                <li>Silahkan pilih “Ya, Saya bersedia.” untuk menggunakan DX Trade untuk transaksi Anda selanjutnya, atau</li>
+                                <li>Silahkan pilih “Pelajari DXTrade” untuk mendapatkan akun trial <i>online trading</i></li>
+                            </ul>
+                                Pada akhir Agustus 2020, DXTrade akan menjadi <i>Official Online Trading</i> PT. Bahana Sekuritas dan akan sepenuhnya menggantikan DTNextG.
+                                <br/><br/>
+                                Terima kasih,<br/>
+                                PT. Bahana Sekuritas<br/><br/>
                             </p>
                             <p className={"p-migrate"}>
                                 Apakah anda yakin untuk Migrasi
@@ -82,10 +89,10 @@ class ModalTrial extends React.Component{
                             <div className={"row"}>
                                 <div className={"text-center col-sm-9"}>
                                     <div className={"text-left"}>
-                                        <Checkbox label='Ya' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} defaultChecked checked={(this.state.selectedMigrate)?true:false}/>
+                                        <Checkbox label='Ya, Saya bersedia' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} defaultChecked checked={(this.state.selectedMigrate)?true:false}/>
                                     </div>
                                     <div className={"text-left"}>
-                                        <Checkbox label='Tidak' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} checked={(this.state.selectedMigrate)?false:true}/>
+                                        <Checkbox label='Pelajari DXTrade' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} checked={(this.state.selectedMigrate)?false:true}/>
                                     </div>
                                 </div>
                                 <div className={"col-sm-3 text-center"}>
@@ -96,29 +103,37 @@ class ModalTrial extends React.Component{
                             </div>
                         </div>
                         <div
-                            className={`px-0 pt-3 col-sm-8 px-3 py-4 ${this.state.selected == 2?"d-block":"d-none"}`}
+                            className={`px-0 pt-3 col-sm-12 col-xs-12 px-3 py-4 ${this.state.selected == 2?"d-block":"d-none"}`}
                         >
-                            <h2 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Notification Alert</h2>
+                            <h3 className={"text-center d-border-bottom-bold-migrate pb-2 b-title"}>Welcome to DX Trade</h3>
                             <br/>
                             <p className={"p-migrate"}>
-                                Thank you for being willing to migrate to the Dxtrade application.<br/>
-                                Here is the information that we have to submit:<br/><br/>
-                                1. Your account will be active starting H +, if you choose agree before 15.00 WIB.<br/>
-                                2. After you activate your DXTrade account, your account cannot be reused in the DT NextG application.<br/>
-                                3. At present the DXTrade application can only be accessed through a browser at the address <a href={"https://dxtrade.bahana.co.id/"} target={"_blank"}>https://dxtrade.bahana.co.id/</a> <br/>
-                                and the DXTrade application cannot be accessed by a mobile app device.<br/>
-                                4. The mobile version of the DXTrade application will be available in September 2020 after obtaining certification from the Market exchange<br/>
+                                Thank you for your trust to continuously use online trading Bahana Sekuritas.<br/><br/>
+                                As a form of our commitment to enhance your conveniences,
+                                hereby we inform that we have reform our online trading platform to DXTrade.
+                                At this mean time, DXTrade only accessible on web browser version.
+                                Other version of DXTrade will be available on September 2020.<br/><br/>
+
+                                Now, you can access DXTrade by using your current User ID for online trading.<br/>
+                                <ul>
+                                    <li>Please choose “Yes, I want DX Trade” to use DXTrade for your next transaction, or</li>
+                                    <li>Please choose “I want to know more” to get your DXTrade trial account</li>
+                                </ul>
+                                By the end of August 2020, DXTrade will officially replacing DTNextG as PT. Bahana Sekuritas online trading.
+                                <br/><br/>
+                                Thank you,<br/>
+                                PT. Bahana Sekuritas<br/><br/>
                             </p>
                             <p className={"p-migrate"}>
-                                Are u sure about migration?
+                                Are you sure to migrate?
                             </p>
                             <div className={"row"}>
                                 <div className={"text-center col-sm-9"}>
                                     <div className={"text-left"}>
-                                        <Checkbox label='Yes' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} defaultChecked checked={(this.state.selectedMigrate)?true:false}/>
+                                        <Checkbox label='Yes, I want DX Trade' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} defaultChecked checked={(this.state.selectedMigrate)?true:false}/>
                                     </div>
                                     <div className={"text-left"}>
-                                        <Checkbox label='No' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} checked={(this.state.selectedMigrate)?false:true}/>
+                                        <Checkbox label='I want to know more' className={"text-black"} onChange={()=>this.setState({selectedMigrate: !this.state.selectedMigrate})} checked={(this.state.selectedMigrate)?false:true}/>
                                     </div>
                                 </div>
                                 <div className={"col-sm-3 text-center"}>
@@ -126,7 +141,7 @@ class ModalTrial extends React.Component{
                                     </button><br/>
                                     <span className={"f-18 text-danger pointer"} onClick={this.migrate}>OK</span>
                                 </div>
-                        </div>
+                            </div>
                         </div>
                         <div
                             className={`px-0 pt-3 col-sm-12 px-3 py-4 text-center ${this.state.selected == 3?"d-block":"d-none"}`}
